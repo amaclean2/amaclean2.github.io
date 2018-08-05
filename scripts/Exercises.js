e2151882a46c251f14863ee010eb26fa45373e27
amaclean <andrew.n.maclean@gmail.com>
2018-08-05 13:08:51 -0700

    dayTwo

--- scripts/Exercises.js
+++ scripts/Exercises.js
@@ -17,39 +17,31 @@ let functions = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eig
 				'TwentyOne', 'TwentyTwo', 'TwentyThree', 'TwentyFour', 'TewntyFive', 'TwentySix', 'TwentySeven', 
 				'TwentyEight', 'TwentyNine', 'Thirty', 'ThirtyOne'];
 
+function httpCall(filePath) {
+	let xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if ( this.readyState === 4 && this.status === 200 ) {
+			myEl.innerHTML += this.responseText;
+		}
+	}
+
+	xhttp.open('GET', filePath, true);
+	xhttp.send();
+}
+
 function AugustFour() {
 
-	myEl.innerHTML += '<div class="focus">let newArr = [5, 4, 3, 2, 1];</div>';
-	myEl.innerHTML += `An array is a data object to group a list of emements. <br />
-					   Each item in an array is called an element.	<br />
-					   In programming you count elements starting from 0, so '5' would be the 0-th element, '4' the first and so on.<br />
-					   You access this by calling newArr[0]. <br />`;
-	myEl.innerHTML += '<div class="focus">let element = newArr[3]; // 2';
-
+	httpCall('html/AugustFour.html');
 }
 
 function AugustFive() {
-	let xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState === 4 && this.status === 200 ) {
-			myEl.innerHTML += this.responseText;
-		}
-	}
 
-	xhttp.open('GET', 'html/AugustFive.html', true);
-	xhttp.send();
+	httpCall('html/AugustFive.html');
 }
 
 function AugustSix() {
-	let xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState === 4 && this.status === 200 ) {
-			myEl.innerHTML += this.responseText;
-		}
-	}
 
-	xhttp.open('GET', 'html/AugustSix.html', true);
-	xhttp.send();
+	httpCall('html/AugustSix.html');
 }
 
 let functionName = months[month] + functions[day];
