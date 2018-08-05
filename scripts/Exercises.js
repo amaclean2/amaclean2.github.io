f77c68ce70d4188fc81650cd7236094d7da43451
amaclean <andrew.n.maclean@gmail.com>
2018-08-05 12:48:06 -0700

    dayTwo

--- scripts/Exercises.js
+++ scripts/Exercises.js
@@ -29,29 +29,6 @@ function AugustFour() {
 }
 
 function AugustFive() {
-	myEl.innerHTML += `<div class="focus">This will be more instructions than a lesson</div>
-					   Go here and download this program.<br />
-					   <a href="https://www.sublimetext.com/3" target="_blank" class="download-button">Sublime</a><br />
-					   Mac OS:<br />
-					   <code class="keys">command + space</code>, type "terminal" press enter, then type the following commands
-					   <div class="center-this"><code>cd ~/Desktop</code><br /><br />
-					   <code>mkdir TheLessonBox</code><br /><br />
-					   <code>cd TheLessonBox</code><br /><br />
-					   <code>ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime</code><br /><br />
-					   <code>sublime .</code><br /><br />
-					   <code class="keys">command + s</code><br /><br />
-					   Save as ExerciseOne.py<br /></div>`;
-					   // Windows:<br />
-					   // I'll have the instructions tomorrow...
-					   // Press the Windows key, type Command Prompt and hit enter
-					   // <div class="center-this"><code>cd ~/Desktop</code><br />
-					   // <code>mkdir TheLessonBox</code><br />
-					   // <code>cd TheLessonBox</code><br />
-					   // <code>ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime</code><br />
-					   // <code>sublime .</code><br /></div>`;
-}
-
-function AugustFive() {
 	let xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState === 4 && this.status === 200 ) {
