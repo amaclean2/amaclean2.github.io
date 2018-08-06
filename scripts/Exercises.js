66334083399f3502c4f628db30e9422565d65afe
amaclean <andrew.n.maclean@gmail.com>
2018-08-06 13:20:09 -0700

    buttons

--- scripts/Exercises.js
+++ scripts/Exercises.js
@@ -1,31 +1,77 @@
-let myEl = document.getElementById('actionBox');
+function renderCalendar(date) {
 
-let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-let today = new Date(),
-	year = today.getFullYear(),
-	month = today.getMonth(),
-	day = today.getDate(),
-	weekday = today.getDay(),
+	today = date;
+	year = today.getFullYear();
+	month = today.getMonth();
+	day = today.getDate();
+	weekday = today.getDay();
 	todayString = days[weekday] + ' ' + months[month] + ' ' + day + ', ' + year;
 
-myEl.innerHTML += '<div class="date">' + todayString + '</div>';
+	functionName = months[month] + functions[day];
 
-let functions = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 
-				'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen', 'Twenty', 
-				'TwentyOne', 'TwentyTwo', 'TwentyThree', 'TwentyFour', 'TewntyFive', 'TwentySix', 'TwentySeven', 
-				'TwentyEight', 'TwentyNine', 'Thirty', 'ThirtyOne'];
+	let dateObj = document.getElementById('date');
 
-let functionName = months[month] + functions[day];
+	if ( dateObj ) {
+		dateObj.innerHTML = todayString;
+	}
+	else {
+		myEl.innerHTML += '<div class="date" id="date">' + todayString + '</div>';
+	}
+}
 
-function httpCall() {
+function showSkipButtons() {
+	let skipButton = document.getElementById('parentButton'),
+		buttonBox = document.getElementById('buttonBox');
+
+	skipButton.classList.add('hidden-buttons');
+	buttonBox.classList.remove('hidden-buttons');
+}
+
+function moveBack() {
+	today.setDate(today.getDate() - 1);
+	httpCall(today);
+
+	document.getElementById('forewardButton').disabled = false;
+
+	let disabled = today.getTime();
+	disabled = new Date(disabled);
+	disabled.setDate(disabled.getDate() - 1);
+	disabled = disabled.getTime();
+	if(disabled < 1533366000000) {
+		document.getElementById('backButton').disabled = true;
+	}
+}
+
+function moveForewards() {
+	today.setDate(today.getDate() + 1);
+	httpCall(today);
+
+	document.getElementById('backButton').disabled = false;
+
+	let disabled = today.getTime();
+	disabled = new Date(disabled);
+	disabled.setDate(disabled.getDate() + 1);
+	disabled = disabled.getTime();
+	if(disabled > new Date().getTime()) {
+		document.getElementById('forewardButton').disabled = true;
+	}
+}
+
+function httpCall(date) {
+
+	renderCalendar(date);
+
 	let xhttp = new XMLHttpRequest(),
 		file = 'html/' + functionName + '.html';
 
 	xhttp.onreadystatechange = function() {
 		if ( this.readyState === 4 && this.status === 200 ) {
-			myEl.innerHTML += this.responseText;
+			let body = document.getElementById('lessonBody');
+			if ( body ) {
+				body.innerHTML = this.responseText;
+			} else {
+				myEl.innerHTML += '<div id="lessonBody">' + this.responseText + '</div>';
+			}
 		}
 	}
 
@@ -33,4 +79,15 @@ function httpCall() {
 	xhttp.send();
 }
 
-httpCall();
+let myEl = document.getElementById('actionBox');
+
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+	days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+	functions = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 
+				'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen', 'Twenty', 
+				'TwentyOne', 'TwentyTwo', 'TwentyThree', 'TwentyFour', 'TewntyFive', 'TwentySix', 'TwentySeven', 
+				'TwentyEight', 'TwentyNine', 'Thirty', 'ThirtyOne'];
+
+let today, year, month, day, weekday, todayString, functionName;
+
+httpCall(new Date());
